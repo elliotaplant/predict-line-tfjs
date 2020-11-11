fde16c924be66919204c8ac957b71d70c844df6e
Elliot Plant <elliotaplant@gmail.com>
2020-11-10 17:21:02 -0800

    Make is user friendly

--- script.js
+++ script.js
@@ -5,16 +5,30 @@ class Brain {
     this.height = 400;
     this.points = [];
     this.learningRate = 0.02;
+    this.momentum = 0.02;
+    this.optimizerName = 'momentum';
     this.learn = this.learn.bind(this);
+    this.numLearns = 0;
+    this.prevCost = 0;
+    this.costDelta = 1;
   }
 
   init() {
+    this.learningRate = JSON.parse(localStorage.getItem('learningRate') || '0.02');
+    autoFit.checked = JSON.parse(localStorage.getItem('autofit') || 'true');
+    this.optimizerName = localStorage.getItem('optimizer') || 'sgd';
     this.m = tf.variable(tf.scalar(random(-1, 1)));
     this.b = tf.variable(tf.scalar(random(0.2, 0.8)));
+    setImmediate(() => {
+      learningRate.value = this.learningRate;
+      optimizerSelect.value = this.optimizerName;
+      optimizerSelect.value = this.optimizerName;
+      renderFitBtn();
+    });
   }
 
   createCanvas() {
-    createCanvas(this.width, this.height);
+    return createCanvas(this.width, this.height);
   }
 
 
@@ -36,16 +50,41 @@ class Brain {
   }
 
   learn() {
+    if (this.points.length <= 0) {
+      return;
+    }
+
     tf.tidy(() => {
-      if (this.points.length <= 0) {
-        return;
-      }
+      let needsNewOpt = !this.optimizer;
+
       if (Number(learningRate.value) !== this.learningRate)  {
         this.learningRate = Number(learningRate.value);
-        this.optimizer = tf.train.sgd(this.learningRate);
+        needsNewOpt = true;
       }
-      this.optimizer.minimize(() => this.loss());
+
+      if (optimizerSelect.value !== this.optimizerName)  {
+        this.optimizerName = optimizerSelect.value;
+        needsNewOpt = true;
+      }
+
+      if (needsNewOpt) {
+        if (this.optimizerName === 'momentum') {
+          this.optimizer = tf.train.momentum(this.learningRate, 0.1);
+        } else {
+          this.optimizer = tf.train[this.optimizerName](this.learningRate);
+        }
+      }
     });
+
+    tf.tidy(() => {
+      const cost = this.optimizer.minimize(() => this.loss(), true).dataSync();
+      this.costDelta = cost[0] - this.prevCost;
+      this.prevCost = cost[0];
+    });
+  }
+
+  closeEnough() {
+    return Math.abs(this.costDelta) < 1e-7;
   }
 
   drawPoints() {
@@ -72,16 +111,19 @@ function toCanvas(x, y) {
 
 function mousePressed() {
   brain.addPoint(mouseX, mouseY);
+  brain.learn();
 }
+
 function keyPressed() {
-  if (keyCode === 32) {
+  // Check spacebar
+  if (!autoFit.checked && keyCode === 32) {
     brain.learn();
   }
 }
 
 function draw() {
   tf.tidy(() => {
-    if (autoFit.checked) {
+    if (autoFit.checked && !brain.closeEnough()) {
       brain.learn();
     }
     background(0);
@@ -92,6 +134,11 @@ function draw() {
 }
 
 function toggleFitBtn() {
+  localStorage.setItem('autofit', autoFit.checked);
+  renderFitBtn();
+}
+
+function renderFitBtn() {
   if (autoFit.checked) {
     learnBtn.style.display = 'none';
   } else {
@@ -99,11 +146,31 @@ function toggleFitBtn() {
   }
 }
 
-const brain = new Brain();
+function updateLearningRate() {
+  localStorage.setItem('learningRate', learningRate.value);
+  brain.learn();
+}
+
+function updateOptimizer() {
+  localStorage.setItem('optimizer', optimizerSelect.value);
+  brain.learn();
+}
+
+let brain = new Brain();
+
+function reset() {
+  brain = new Brain();
+  brain.init();
+}
 
 function setup() {
   brain.init();
-  brain.createCanvas();
+  const canvas = brain.createCanvas();
+  canvas.parent('canvasHolder');
+
   learnBtn.onclick = brain.learn;
+  resetBtn.onclick = reset;
   autoFit.onchange = toggleFitBtn;
+  learningRate.onchange = updateLearningRate;
+  optimizerSelect.onchange = updateOptimizer;
 }
